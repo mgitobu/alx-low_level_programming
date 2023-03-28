8418318ead2a27abecc11a031616d1f05f4a76f8
mgitobu <mgitobu5@gmail.com>
2023-03-28 02:08:54 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 /**
- *main - entry point
- */
-int main(void)
+*main - entry point
+*
+*Return: 0 number
+*/
+int main (void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
